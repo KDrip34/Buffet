9a5fa2052e591a497be5fc8f6477f4a1899a97c2
KDrip34 <124319502+KDrip34@users.noreply.github.com>
2023-07-28 15:48:48 -0700

    Update and rename .gitignore to buffet.py

---
+++ buffet.py
@@ -0,0 +1,9 @@
+buffet = ('sushi' , 'steak' , 'sandwhiches' , 'mexicanfood' , 'breakfast')
+print("Original buffet menu":)
+for buffet in buffet:
+	print(buffet)
+print(buffet)
+buffet = ('icecream' , 'pasta' , 'asian' , 'indian')
+print("\nModified buffet:")
+for buffet in buffet:
+	print(buffet)
